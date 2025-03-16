fixed softmax and least squares, ready to implement softmax with no activation

Framework/loss_functions/least_squares.py
@@ -3,4 +3,4 @@ import numpy as np
 class LeastSquaresLoss:
     @staticmethod
     def compute(y_true, y_pred):
-      return np.mean(np.log(1 + np.exp(-y_true * z)))
+      return 0.5 * np.mean((y_true - y_pred) ** 2)